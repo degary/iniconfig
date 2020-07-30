46cd4caf97cda11d1c3c7b7da5d6d9481b9cf365
邓辉 <denghui@blhcn.com>
2020-07-30 09:35:17 +0800

    First Commit

---
+++ ini_config.go
@@ -0,0 +1,9 @@
+package iniconfig
+
+func Marshal(data interface{}) (result []byte, err error) {
+	return
+}
+
+func UnMarshal(data []byte, result interface{}) (err error) {
+	return
+}
---
+++ ini_config_test.go
@@ -0,0 +1,41 @@
+package iniconfig
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+type Config struct {
+	ServerConf ServerConfig `ini:"server"`
+	MysqlConf  MysqlConfig  `ini:"mysql"`
+}
+
+type ServerConfig struct {
+	Ip   string
+	Port int
+}
+
+type MysqlConfig struct {
+	UserName string `ini:"username"`
+	Passwd   string `ini:"passwd"`
+	Database string `ini:"database"`
+	Host     string `ini:"host"`
+	Port     int    `ini:"port"`
+}
+
+func TestIniConfig(t *testing.T) {
+	fmt.Println("hello")
+	data, err := ioutil.ReadFile("./config.ini")
+	if err != nil {
+		t.Error("read file failed")
+	}
+
+	var conf Config
+	err = UnMarshal(data, &conf)
+	if err != nil {
+		t.Errorf("unmarshal failed,err:%v", err)
+	}
+
+	t.Logf("unmarshal success, conf:%#v", conf)
+}
